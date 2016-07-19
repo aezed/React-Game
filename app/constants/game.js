0126cff00cc70d49a5d75827a629400e36d76202
Austin Baltes <austinba@gmail.com>
2016-07-18 21:03:19 -0700

    added constants into game.js

--- app/constants/game.js
+++ app/constants/game.js
@@ -1,2 +1,7 @@
-export const gameTickSize = 250;
-export const gridSize = 10;
+export const boardHeight = 500;
+export const boardWidth = 500;
+export const paddleMovementRadius = 200;
+export const ballRadius = 15;
+export const paddleHeight = 20;
+export const paddleWidth = 100;
+export const intervalTime = 30;
