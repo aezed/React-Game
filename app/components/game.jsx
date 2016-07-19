1039fe5fe6a5aa34795464966cc18df772f458f7
Austin Baltes <austinba@gmail.com>
2016-07-18 20:41:15 -0700

    paddle position via keys

--- app/components/game.jsx
+++ app/components/game.jsx
@@ -1,60 +1,39 @@
 import React from 'react';
 import keys from '../constants/keys';
-import {gameTickSize} from '../constants/game'
+// import {gameTickSize} from '../constants/game'
 import _ from 'lodash';
 
-import Grid from './Grid.jsx';
+// import Grid from './Grid.jsx';
 
 export default class Game extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            gameTime: 0,
-            position: [0, 0],
-            color: 'red'
+            paddlePosition: 1
         }
     }
-    componentDidMount(){
+    componentDidMount() {
         this.handleKeyDown();
-
-        setInterval(()=>{
-            this.setState({gameTime: this.state.gameTime + gameTickSize })
-        }, gameTickSize)
     }
     handleKeyDown(e){
         window.addEventListener('keydown', (e)=>{
-            var position = _.clone(this.state.position);
+            var paddlePosition = _.clone(this.state.paddlePosition);
             var color = this.state.color;
             switch (e.keyCode){
                 case keys.LEFT:
-                    --position[0];
+                    paddlePosition -= 0.05;
                     break;
                 case keys.RIGHT:
-                    ++position[0];
-                    break;
-                case keys.DOWN:
-                    ++position[1];
-                    break;
-                case keys.UP:
-                    --position[1];
-                    break;
-                case keys.SPACEBAR:
-                    color = _.sample(['red', 'green', 'blue', 'yellow']);
-                    break;
-                case keys.ENTER:
+                    paddlePosition -= 0.05;
                     break;
             }
-            this.setState({position, color});
+            this.setState({paddlePosition});
         });
     }
     render(){
         return (
             <div>
-                { this.state.gameTime }
-                <Grid 
-                    position={this.state.position} 
-                    color={this.state.color}
-                />
+                { this.state.paddlePosition }
             </div>
         )
     }
